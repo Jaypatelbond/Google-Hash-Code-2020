4b20a9defa67105913bdce4752d5fb62cf4ea81c
Jay Patel <Jaypatelbond7@gmail.com>
2020-01-23 18:46:47 +0530

    Update
    d and e still not working. Java Heap Problem. Have to look up the Heap Dump and overlook the code.

--- Submission in Java/GoogleHashCode2020/src/Main.java
+++ Submission in Java/GoogleHashCode2020/src/Main.java
@@ -1,15 +1,15 @@
-import sun.nio.cs.ext.MacArabic;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Arrays;
-
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * @author Jaypatelbond
@@ -18,131 +18,133 @@ import java.util.Set;
  */
 
 public class Main {
-    public static int maxPizzaSlices;
-    public static int typesofPizza;
+    private static final String inputSource = "src/Input Files/a_example";
+    private static final String outputSource = "src/Output Files/a_example";
+    private static final Path inputFile = Paths.get(inputSource + ".in");
+    private static final Path outputFile = Paths.get(outputSource + ".out");
 
-    // array2D[i][j] is going to store true if sum j is
-    // possible with array elements from 0 to i.
-    public static boolean[][] array2D;
 
-    static void createOutputFile(Set<Integer> v) {
-        System.out.println("v = " + v);
-        int sum = 0;
-        for( int num : v) {
-            sum += num;
-        }
-        System.out.println("v = " + sum);
-        // Create output file.
-        String outputFile = "src/Output Files/";
-        String list = Arrays.toString(v.toArray()).replace("[", "").replace("]", "").replace(",", "");
-        System.out.println("v = " + list);
-        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputFile, "b_small.out"))) {
-            for (int i = 0; i < 1; i++) {
-                writer.write("" + v.size());
-            }
-            writer.newLine();
-            for (int i = v.size(); i == v.size(); i++) {
-                writer.write(list);
-                writer.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // A recursive function to print all subsets with the
-    // help of array2D[][]. Vector p[] stores current subset.
-    static void printSubsetRecursion(int[] arr, int i, int sum, Set<Integer> p) {
-
-        // If we reached end and sum is non-zero. We print
-        // p[] only if arr[0] is equal to sum OR array2D[0][sum]
-        // is true.
-        if (i == 0 && sum != 0 && array2D[0][sum]) {
-            p.add(arr[i]);
-            createOutputFile(p);
-            p.clear();
-            return;
-        }
-
-        // If sum becomes 0
-        if (i == 0 && sum == 0) {
-            createOutputFile(p);
-            p.clear();
-            return;
-        }
-
-        // If given sum can be achieved after ignoring
-        // current element.
-        if (array2D[i - 1][sum]) {
-            Set<Integer> b = new HashSet<>(p);
-            printSubsetRecursion(arr, i - 1, sum, b);
-        }
-
-        // If given sum can be achieved after considering
-        // current element.
-        if (sum >= arr[i] && array2D[i - 1][sum - arr[i]]) {
-            p.add(arr[i]);
-            printSubsetRecursion(arr, i - 1, sum - arr[i], p);
-        }
-    }
-
-    // Prints all subsets of arr[0..pizzaType-1] with sum 0.
-    static void printAllSubsets(int[] arr, int pizzaType, int sum) {
-        if (pizzaType == 0 || sum < 0)
-            return;
-
-        // Sum 0 can always be achieved with 0 elements
-        array2D = new boolean[pizzaType][sum + 1];
-        for (int i = 0; i < pizzaType; ++i) {
-            array2D[i][0] = true;
-        }
-
-        // Sum arr[0] can be achieved with single element
-        if (arr[0] <= sum)
-            array2D[0][arr[0]] = true;
-
-        // Fill rest of the entries in array2D[][]
-        for (int i = 1; i < pizzaType; ++i)
-            for (int j = 0; j < sum + 1; ++j)
-                array2D[i][j] = (arr[i] <= j) ? (array2D[i - 1][j] || array2D[i - 1][j - arr[i]]) : array2D[i - 1][j];
-        if (!array2D[pizzaType - 1][sum]) {
-            System.out.println("There are no subsets with" + " sum " + sum);
-            return;
-        }
-
-        // Now recursively traverse array2D[][] to find all
-        // paths from array2D[n-1][sum]
-        Set<Integer> p = new HashSet<Integer>();
-        printSubsetRecursion(arr, pizzaType - 1, sum, p);
-    }
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
         Instant start = Instant.now();
-        //Read Input File.
-        String inputFile = "src/Input Files/b_small";
-
-        BufferedReader reader = new BufferedReader(new FileReader(inputFile + ".in"));
-
-        String line, firstLine;
-        firstLine = reader.readLine();
-
-        String[] values;
-        values = firstLine.split(" ");
-
-        maxPizzaSlices = Integer.parseInt(values[0]);
-        typesofPizza = Integer.parseInt(values[1]);
-
-        while ((line = reader.readLine()) != null) {
-            String[] types = line.split(" ");
-            int[] array = Arrays.stream(types).mapToInt(Integer::parseInt).toArray();
-            int pizzaType = types.length;
-            printAllSubsets(array, pizzaType, maxPizzaSlices);
-        }
+        Main main = new Main();
+        main.startProblem();
 
         Instant finish = Instant.now();
-        System.out.println("Execution time:" + Duration.between(start, finish).getSeconds() + " sec.");
         System.out.println("Execution time:" + Duration.between(start, finish).toMillis() + " ms.");
 
     }
-}
+
+    private void startProblem() {
+        Main.Input input = readInputSource();
+        input.print();
+        Set<Integer> result = new HashSet<>();
+        int[][] memo = getMemoMaze(input);
+
+        System.out.println("Max possible Weight: " + ZeroOneKnapsack(input.getMaxSlices(), input.getTypesofPizza(), input.getSlices(), memo, result));
+
+        System.out.println(Arrays.toString(result.toArray()));
+        System.out.println(result.stream().map(index -> input.getSlices()[index]).reduce(Integer::sum));
+        writeResultToFile(result);
+    }
+
+    private int[][] getMemoMaze(Input input) {
+        int[][] memo = new int[input.getTypesofPizza() + 1][input.getMaxSlices() + 1];
+        for (int j = 0; j <= input.getTypesofPizza(); j++) {
+            Arrays.fill(memo[j], 0, input.getMaxSlices() + 1, -1);
+        }
+        return memo;
+    }
+
+    private int ZeroOneKnapsack(int maxWeight, int numberOfElements, int[] weights, int[][] memo, Set<Integer> result) {
+        if (numberOfElements == 0 || maxWeight == 0) {
+            return 0;
+        }
+        if (memo[numberOfElements][maxWeight] == -1) {
+            if (weights[numberOfElements - 1] > maxWeight) {
+                memo[numberOfElements][maxWeight] = ZeroOneKnapsack(maxWeight, numberOfElements - 1, weights, memo, result);
+
+            } else {
+                Set<Integer> resultWithElement = new HashSet<Integer>();
+                Set<Integer> resultWithoutElement = new HashSet<Integer>();
+                int weightWithElement = weights[numberOfElements - 1] + ZeroOneKnapsack(maxWeight - weights[numberOfElements - 1], numberOfElements - 1,
+                        weights, memo, resultWithElement);
+                int weightWithoutElement = ZeroOneKnapsack(maxWeight, numberOfElements - 1, weights, memo, resultWithoutElement);
+                if (weightWithElement >= weightWithoutElement) {
+                    result.addAll(resultWithElement);
+                    result.add(numberOfElements - 1);
+                    memo[numberOfElements][maxWeight] = weightWithElement;
+                } else {
+                    result.addAll(resultWithoutElement);
+                    memo[numberOfElements][maxWeight] = weightWithoutElement;
+                }
+            }
+        }
+        return memo[numberOfElements][maxWeight];
+        }
+
+
+        private Input readInputSource () {
+            try (BufferedReader reader = Files.newBufferedReader(inputFile, StandardCharsets.UTF_8)) {
+                Main.Input input = new Input();
+                String[] firstLine = reader.readLine().split(" ");
+                input.setMaxSlices(Integer.parseInt(firstLine[0]));
+                input.setTypesofPizza(Integer.parseInt(firstLine[1]));
+                input.setSlices(stringArrayToIntArray(reader.readLine().split(" ")));
+                return input;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        int[] stringArrayToIntArray (String[]stringArray){
+            return Stream.of(stringArray).mapToInt(Integer::parseInt).toArray();
+        }
+
+        private void writeResultToFile (Set < Integer > result) {
+            StringBuilder outputFileBuilder = new StringBuilder();
+            outputFileBuilder.append(result.size() + "\n");
+            result.forEach(item -> outputFileBuilder.append(item + " "));
+
+            try (BufferedWriter writer = Files.newBufferedWriter(outputFile)) {
+                writer.write(outputFileBuilder.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private static class Input {
+            private int maxSlices; // Max Weights
+            private int typesofPizza; // Number of Element
+            private int[] slices;  // Weights
+
+            public int getMaxSlices() {
+                return maxSlices;
+            }
+
+            public void setMaxSlices(int maxSlices) {
+                this.maxSlices = maxSlices;
+            }
+
+            public int getTypesofPizza() {
+                return typesofPizza;
+            }
+
+            public void setTypesofPizza(int typesofPizza) {
+                this.typesofPizza = typesofPizza;
+            }
+
+            public int[] getSlices() {
+                return slices;
+            }
+
+            public void setSlices(int[] slices) {
+                this.slices = slices;
+            }
+
+            public void print() {
+                System.out.println("maxSlices = " + maxSlices + "\ntypesofElement = " + typesofPizza + "\nSlices = " + Arrays.toString(slices) + "\n");
+            }
+        }
+    }
